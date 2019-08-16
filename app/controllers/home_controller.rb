18c58eb12910e7ec68959851245df3885f86f082
PhamHaiDang <pham.hai.dang@sun-asterisk.com>
2019-08-16 15:35:15 +0700

    Show_newspapers_home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @newspaper_hots = Newspaper.ordered_by_newspaper_id.take(Settings.show_newspaper_home)
+    @newspaper_normals = Newspaper.by_status_normal.take(Settings.show_newspaper_home)
   end
 end
