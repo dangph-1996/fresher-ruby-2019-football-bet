e3651d8ea6cbe47e6316107f7584f7241b3621c1
PhamHaiDang <pham.hai.dang@sun-asterisk.com>
2019-08-16 15:35:15 +0700

    Improve_module_comment

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,16 +1,4 @@
 $(document).on('turbolinks:load', function() {
-  $( '#content_submit' ).click(function() {
-    if ($('#comment_content').val()== '') {
-      alert(I18n.t("no_comment"));
-    }
-  });   
-
-  $("#content_submit").click(function() {
-    if ($('#comment_content').val() == "") {
-      alert("Comment not empty")
-    }
-  });
-
   $("#select_league, #select-box-season, #select-box-round").on("change", function() {
     var league = $("#select_league").val();
     var season = $("#select_season").val();
