fd290e60eda1209844454333c14ea0fbe2126505
PhamHaiDang <pham.hai.dang@sun-asterisk.com>
2019-09-10 15:53:30 +0700

    Module bet

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,20 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   has_many :comments, dependent: :destroy
+  has_many :bets, dependent: :destroy
   mount_uploader :avatar, ImageUploader
 
-  class << self
-    def new_with_session params, session
-      tap do |user|
-        if data = session["devise.facebook_data"] &&
-          session[:devise.facebook_data][:extra][:raw_info]
-          user.email = data["email"] if user.email.blank?
-        end
+  def new_with_session params, session
+    tap do |user|
+      if data = session["devise.facebook_data"] &&
+        session[:devise.facebook_data][:extra][:raw_info]
+        user.email = data["email"] if user.email.blank?
       end
     end
+  end
 
-    def from_omniauth auth
+  class << self
+    def from_omniauth auth      
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0, 20]
